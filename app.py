080f936d6a08420f410449544e96124eb6f45303
Rahul-s-007 <rahulrtg7703@gmail.com>
2025-02-22 18:31:12 +0530

    added fuc for secrets

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
+from streamlit import secrets
 
 from openai import OpenAI
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 ReAct_prompt_template = """
 You run in a loop of Thought, Action, PAUSE, Action_Response.
